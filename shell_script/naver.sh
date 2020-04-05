db2abf4cad68eecc5e920806cee9766b8927e5ab
xen-ubuntu <yoonsung.kim87@gmail.com>
2020-04-05 15:30:55 +0900

    minor

--- shell_script/naver.sh
+++ shell_script/naver.sh
@@ -1,1 +1,1 @@
-curl -X GET 'polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:'$(cat ./list.txt) | jq '.result.areas | .[0].datas'
+curl -X GET 'polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:'$(cat /tmp/stock_trade/shell_script/list.txt) | jq '.result.areas | .[0].datas'
