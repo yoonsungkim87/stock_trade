0f828f7fc7e2bf6b764bd514424ad46f577832e0
xen-ubuntu <yoonsung.kim87@gmail.com>
2020-12-16 20:41:01 +0900

    naver_api_update

--- shell_script/naver.sh
+++ shell_script/naver.sh
@@ -1,1 +1,1 @@
-curl -X GET 'polling.finance.naver.com/api/realtime.nhn?query=SERVICE_ITEM:'$(cat /tmp/stock_trade/shell_script/list.txt) | jq '.result.areas | .[0].datas'
+curl -X GET 'https://finance.naver.com/api/sise/etfItemList.nhn' | jq '.result.etfItemList'
